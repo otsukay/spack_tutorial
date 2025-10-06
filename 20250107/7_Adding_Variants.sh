e334971cd4e39d76c3c1dc62cbf7ca51b6cdd756
Yuichi Otsuka <otsukay@riken.jp>
2025-10-06 23:03:19 +0900

    add

---
+++ 20250107/7_Adding_Variants.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#spack edit tutorial-mpileaks
+cp ~/spack_tutorial/20250107/tutorial-mpileaks/4.py $HOME/my_pkgs/spack_repo/tutorial/packages/tutorial_mpileaks/package.py
+
+spack install --verbose tutorial-mpileaks stackstart=4
