44023a4d1e5199b613a20109aaa8a1cf1326ba8a
Yuichi Otsuka <otsukay@riken.jp>
2025-10-07 00:02:25 +0900

    add

--- 20251007/0_setup-spack.sh
+++ 20251007/0_setup-spack.sh
@@ -1,6 +1,4 @@
 
-git clone https://github.com/otsukay/spack_tutorial.git
-
 git clone --depth=2 --branch=releases/v1.0 https://github.com/spack/spack
 . spack/share/spack/setup-env.sh
 spack repo update builtin --tag v2025.07.0
