315edafdeb3c5f431aea4e94f79714793987b0f3
Yuichi Otsuka <otsukay@riken.jp>
2025-10-06 23:42:09 +0900

    add

--- 20250107/6_Specifying_Configure_Arguments.sh
+++ 20250107/6_Specifying_Configure_Arguments.sh
@@ -3,4 +3,6 @@
 #spack edit tutorial-mpileaks
 cp ~/spack_tutorial/20250107/tutorial-mpileaks/3.py $HOME/my_pkgs/spack_repo/tutorial/packages/tutorial_mpileaks/package.py
 
+git diff ~/spack_tutorial/20250107/tutorial-mpileaks/2.py ~/spack_tutorial/20250107/tutorial-mpileaks/3.py
+
 spack install tutorial-mpileaks
