372659b9c6dc803960681593cc30685af980e068
Yuichi Otsuka <otsukay@riken.jp>
2025-10-07 00:00:49 +0900

    add

--- 20251007/0_setup-spack.sh
+++ 20251007/0_setup-spack.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+
+git clone https://github.com/otsukay/spack_tutorial.git
 
 git clone --depth=2 --branch=releases/v1.0 https://github.com/spack/spack
-source spack/share/spack/setup-env.sh
+. spack/share/spack/setup-env.sh
 spack repo update builtin --tag v2025.07.0
 spack mirror add tutorial /mirror
 spack buildcache keys --install --trust
 spack bootstrap now
 spack compiler find
 
-export EDITOR=emacs
+#export EDITOR=emacs
