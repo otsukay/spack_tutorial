05caa91f0065eaa19887d11990cb8cbc0ab7b5ec
Yuichi Otsuka <otsukay@riken.jp>
2025-10-06 23:48:22 +0900

    add

--- 20250107/8_Adding_Tests.sh
+++ 20250107/8_Adding_Tests.sh
@@ -3,11 +3,15 @@
 #spack edit tutorial-mpileaks
 cp ~/spack_tutorial/20250107/tutorial-mpileaks/5.py $HOME/my_pkgs/spack_repo/tutorial/packages/tutorial_mpileaks/package.py
 
+git diff ~/spack_tutorial/20250107/tutorial-mpileaks/4.py ~/spack_tutorial/20250107/tutorial-mpileaks/5.py
+
 spack uninstall -ay tutorial-mpileaks
 spack install --test=root tutorial-mpileaks
 
 #spack edit tutorial-mpileaks
 cp ~/spack_tutorial/20250107/tutorial-mpileaks/6.py $HOME/my_pkgs/spack_repo/tutorial/packages/tutorial_mpileaks/package.py
+
+git diff ~/spack_tutorial/20250107/tutorial-mpileaks/5.py ~/spack_tutorial/20250107/tutorial-mpileaks/6.py
 
 spack install --test=root tutorial-mpileaks
 
